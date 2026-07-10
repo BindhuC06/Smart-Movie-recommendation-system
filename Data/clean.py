9ad100f7581010d1c79bac01601776a549317864
BindhuC06 <bindulahari2006@gmail.com>
2026-07-10 21:05:20 +0530

    Updated clean.py

--- Data/clean.py
+++ Data/clean.py
@@ -1,13 +1,18 @@
 import pandas as pd
 
 df = pd.read_csv("tmdb_movies_5000.csv")
-
+# dropping rows with empty values
 df.dropna(subset=["overview"], inplace=True)
+# removing extra spaces 
 df = df[df["overview"].str.strip() != ""]
+#Deduplication
 df.drop_duplicates(subset=["title"], inplace=True)
+# String fromatting
 df["genres"] = df["genres"].str.lower()
 df["genres"] = df["genres"].str.replace(", ", ",", regex=False)
+# Filling the missing values
 df["vote_average"] = df["vote_average"].fillna(0)
+# Resetting index to make the data uniform 
 df.reset_index(drop=True, inplace=True)
+#saving back the refined dataset to a new csv file
 df.to_csv("tmdb_movies_cleaned.csv", index=False)
-
